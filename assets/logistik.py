f723f9b32a47fabdaa6a101c21957e3edefbcfd2
umargtg1234-coder <umargtg1234@gmail.com>
2025-11-20 15:15:37 +0700

    b

--- assets/logistik.py
+++ assets/logistik.py
@@ -1,1 +1,10 @@
 # Kode bagian logistik
+class barang():
+    def __init__(self, idbarang, namabarang, kategori, stok, satuan, hargasatuan, kaadaluarsa):
+
+class supplier():
+    def __init__(self, namaspl, alamatspl, telpspl, email):
+
+
+class beli():
+    def __init__(self, )
